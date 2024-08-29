b3afc0b1e6bc45c74a85b6cd05de7065517b366d
Nayara <nayara.evangelista93@gmail.com>
2024-08-28 20:36:10 -0400

    codigo rev

---
+++ Hellor_word_aula_4.c
@@ -0,0 +1,16 @@
+
+// Autor: Mateus/Felix
+// Data: 28/08/2024
+// Modulo: Lógica de Programação
+// Curso: Eletrônica 4.0
+// Local: INDT
+
+
+#include <stdio.h>
+
+int main (void)
+
+{ 
+printf (" Hello, world\n");
+return 0;
+}
